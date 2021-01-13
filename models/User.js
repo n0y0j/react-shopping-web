fc23d56777f7b065a30a6f5708f2ded815683b4b
n0y0j <n0y0j77@gmail.com>
2021-01-13 21:38:55 +0900

    feat: Encrypt password using Bcrypt.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10; // 글자 개수
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +31,25 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+userSchema.pre('save', function( next ) {
+    var user = this
+
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            // hash: 암호화된 비밀번호
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })    
+    }
+
 })
 
 const User = mongoose.model('User', userSchema)
