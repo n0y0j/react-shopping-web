d86078bdd9ed08a791cc4cc7aa72dae956bdeeba
n0y0j <n0y0j77@gmail.com>
2021-01-13 10:35:56 +0900

    feat: Connect MongoDB with mongoose.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://admin:asd123456@cluster0.7qq4l.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
