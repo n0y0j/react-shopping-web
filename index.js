ca0ea2acfee632a48e1f5ade3331f4f3cee98732
n0y0j <n0y0j77@gmail.com>
2021-01-13 20:57:09 +0900

    feat: Implement register with body-parser.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:asd123456@cluster0.7qq4l.mongodb.net/test?retryWrites=true&w=majority', {
@@ -11,6 +19,17 @@ mongoose.connect('mongodb+srv://admin:asd123456@cluster0.7qq4l.mongodb.net/test?
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입 시 필요한 정보를 client에서 가져와 그것들을 데이터 베이스에 저장
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({success: true})
+  })
+
 })
 
 app.listen(port, () => {
