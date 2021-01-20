e451012ebf680c22fae4e26cb365bdd0e4edbf3e
n0y0j <n0y0j77@gmail.com>
2021-01-20 19:10:08 +0900

    feat: Send image to server using axios.

---
+++ server/routes/product.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+
+
+
+module.exports = router;
