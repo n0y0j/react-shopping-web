db065d5f4aefa935486fdfd3e0df6b9f51647e61
n0y0j <n0y0j77@gmail.com>
2021-01-21 11:51:04 +0900

    feat: Save product data in DB.

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import { Typography, Button, Form, Input } from 'antd'
-import FileUpload from '../../utils/FileUpload';
+import React, { useState } from "react";
+import { Typography, Button, Form, Input } from "antd";
+import FileUpload from "../../utils/FileUpload";
+import Axios from "axios";
 
-const { TextArea } = Input
+const { TextArea } = Input;
 
 const Continents = [
   { key: 1, value: "Africa" },
@@ -14,70 +15,100 @@ const Continents = [
   { key: 7, value: "Antarctica" },
 ];
 
-function UploadProductPage() {
+function UploadProductPage(props) {
+  const [Title, setTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Price, setPrice] = useState(0);
+  const [Continent, setContinent] = useState(1);
+  const [Images, setImages] = useState([]);
 
-    const [Title, setTitle] = useState("")
-    const [Description, setDescription] = useState("")
-    const [Price, setPrice] = useState(0)
-    const [Continent, setContinent] = useState(1)
-    const [Images, setImages] = useState([])
+  const titleChangeHandler = (event) => {
+    setTitle(event.currentTarget.value);
+  };
 
-    const titleChangeHandler = (event) => {
-        setTitle(event.currentTarget.value)
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.currentTarget.value);
+  };
+
+  const priceChangeHandler = (event) => {
+    setPrice(event.currentTarget.value);
+  };
+
+  const continentChangeHandler = (event) => {
+    setContinent(event.currentTarget.value);
+  };
+
+  const updateImage = (newImages) => {
+    setImages(newImages);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!Title || !Description || !Price || !Continent || !Images) {
+      return alert("모든 값을 입력해주세요");
     }
 
-    const descriptionChangeHandler= (event) => {
-        setDescription(event.currentTarget.value)
-    }
+    // 서버에 채운 값들을 request로 보낸다.
 
-    const priceChangeHandler = (event) => {
-        setPrice(event.currentTarget.value)
-    }
-    
-    const continentChangeHandler = (event) => {
-        setContinent(event.currentTarget.value)
-    }
+    const body = {
+      writer: props.user.userData._id,
+      title: Title,
+      description: Description,
+      price: Price,
+      images: Images,
+      continents: Continent,
+    };
 
-    const updateImage = (newImages) => {
-      setImages(newImages)
-    }
+    Axios.post("/api/product", body)
+      .then((response) => {
+      if (response.data.success) {
+        alert("상품 업로드에 성공 했습니다.");
+        props.history.push('/')
+      } else {
+        alert("상품 업로드에 실패 했습니다.");
+      }
+    });
+  };
 
-    return (
-      <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
-        <div style={{ textAlign: "center", marginBottom: "2rem" }}>
-          <h2>여행 상품 업로드</h2>
-        </div>
-
-        <Form>
-          {/* DropZone */}
-          <FileUpload refreshFunction={updateImage}/>
-          <br />
-          <br />
-          <label>이름</label>
-          <Input onChange={titleChangeHandler} value={Title} />
-          <br />
-          <br />
-          <label>설명</label>
-          <TextArea onChange={descriptionChangeHandler} value={Description} />
-          <br />
-          <br />
-          <label>가격</label>
-          <Input type="number" onChange={priceChangeHandler} value={Price} />
-          <br />
-          <br />
-          <select onChange={continentChangeHandler} value={Continent}>
-            {Continents.map((item) => (
-              <option key={item.key} value={item.key}>
-                {item.value}
-              </option>
-            ))}
-          </select>
-          <br />
-          <br />
-          <Button>확인</Button>
-        </Form>
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <h2>여행 상품 업로드</h2>
       </div>
-    );
+
+      <Form onSubmit={submitHandler}>
+        {/* DropZone */}
+        <FileUpload refreshFunction={updateImage} />
+        <br />
+        <br />
+        <label>이름</label>
+        <Input onChange={titleChangeHandler} value={Title} />
+        <br />
+        <br />
+        <label>설명</label>
+        <TextArea onChange={descriptionChangeHandler} value={Description} />
+        <br />
+        <br />
+        <label>가격</label>
+        <Input type="number" onChange={priceChangeHandler} value={Price} />
+        <br />
+        <br />
+        <select onChange={continentChangeHandler} value={Continent}>
+          {Continents.map((item) => (
+            <option key={item.key} value={item.key}>
+              {item.value}
+            </option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <Button htmlType='submit'>
+          확인
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default UploadProductPage
+export default UploadProductPage;
