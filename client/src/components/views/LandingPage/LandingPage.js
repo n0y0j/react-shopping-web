03481d275e0e3e296c398d64a27b4acbbd843a18
n0y0j <n0y0j77@gmail.com>
2021-01-31 11:16:24 +0900

    feat: Implement see more button.

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,6 +9,7 @@ function LandingPage() {
     const [Products, setProducts] = useState([])
     const [Skip, setSkip] = useState(0)
     const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState(0)
 
     useEffect(() => {
 
@@ -16,16 +17,9 @@ function LandingPage() {
             skip: Skip,
             limit: Limit
         }
+
+        getProducts(body)
         
-        axios.post('api/product/products', body)
-            .then(response => {
-                if (response.data.success) {
-                    setProducts(response.data.productInfo)
-                }
-                else {
-                    alert(" 상품들을 가져오는데 실패 했습니다.")
-                }
-            })
     }, [])
 
     const renderCards = Products.map((product, index) => {
@@ -40,32 +34,62 @@ function LandingPage() {
       
     });
 
+    const getProducts = (body) => {
+        axios.post('api/product/products', body)
+        .then(response => {
+            if (response.data.success) {
+                if (body.loadMore) {
+                    setProducts([...Products, ...response.data.productInfo])
+                }
+                else {
+                    setProducts(response.data.productInfo)
+                }
+                setPostSize(response.data.postSize)
+            }
+            else {
+                alert(" 상품들을 가져오는데 실패 했습니다.")
+            }
+        })
+    }
+
     const loadMoreHandler = () => {
 
+        let skip = Skip + Limit
+        
+        let body = {
+            skip: skip,
+            limit: Limit,
+            loadMore: true
+        }
+        getProducts(body)
+        setSkip(skip)  
     }
 
     return (
-        <div style={{ width: '75%', margin: '3rem auto' }}>
-            <div style={{ textAlign: 'center' }}>
-                <h2>Let`s Travel Anywhere <Icon type="rocket" /></h2>
-            </div>
-            
-            {/* Filter */}
-
-            {/* Search */}
-
-            {/* Cards */}
-
-            <Row gutter={[16, 16]}>
-                {renderCards}
-            </Row>
-           
-
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button onClick={loadMoreHandler}>더보기</button>
-            </div>
+      <div style={{ width: "75%", margin: "3rem auto" }}>
+        <div style={{ textAlign: "center" }}>
+          <h2>
+            Let`s Travel Anywhere <Icon type="rocket" />
+          </h2>
         </div>
-    )
+
+        {/* Filter */}
+
+        {/* Search */}
+
+        {/* Cards */}
+
+        <Row gutter={[16, 16]}>{renderCards}</Row>
+
+        <br />
+
+        {PostSize >= Limit && (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button onClick={loadMoreHandler}>더보기</button>
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default LandingPage
--- server/routes/product.js
+++ server/routes/product.js
@@ -49,7 +49,7 @@ router.post('/products', (req, res) => {
     
   // product collection에 있는 모든 상품 정보 가져오기
 
-  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let limit = req.body.limit ? parseInt(req.body.limit) : 20;
   let skip = req.body.skip ? parseInt(req.body.skip) : 0;
 
   Product.find()
@@ -58,7 +58,7 @@ router.post('/products', (req, res) => {
     .limit(limit)
     .exec((err, productInfo) => {
       if (err) return res.status(400).json({ success: false, err });
-      return res.status(200).json({ success: true, productInfo });
+      return res.status(200).json({ success: true, productInfo, postSize: productInfo.length });
     });
 
 })
