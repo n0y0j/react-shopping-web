143217150dcdf37743117ea09585bbdba3c9ac94
n0y0j <n0y0j77@gmail.com>
2021-02-09 18:59:13 +0900

    feat: View products in Cart

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_actions";
+import UserCartBlock from "./Sections/UserCartBlock.js"
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -19,7 +20,14 @@ function CartPage(props) {
     }
   }, [props.user.userData]);
 
-  return <div>CartPage</div>;
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <h1>My Cart</h1>
+      <div>
+        <UserCartBlock products={props.user.cartDetail && props.user.cartDetail.product} />
+      </div>
+    </div>
+  );
 }
 
 export default CartPage;
---
+++ client/src/components/views/CartPage/Sections/UserCartBlock.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./UserCartBlock.css"
+
+function UserCartBlock(props) {
+
+    const renderCartImage = (images) => {
+        if (images.length > 0) {
+            let image = images[0]
+            return `http://localhost:5000/${image}`
+        }
+    }
+
+    const renderItems = () => (
+        props.products && props.products.map(product => (
+            <tr>
+                <td>
+                    <img style={{ width: '70px'}} alt="product" src={renderCartImage(product.images)} />
+                </td>
+                <td>
+                    {product.quantity} EA
+                </td>
+                <td>
+                    $ {product.price}
+                </td>
+                <td>
+                    <button>Remove</button>
+                </td>
+            </tr>
+        ))
+    )
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product Image</th>
+                        <th>Product Quantity</th>
+                        <th>Product Price</th>
+                        <th>Remove from Cart</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {renderItems()}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default UserCartBlock
