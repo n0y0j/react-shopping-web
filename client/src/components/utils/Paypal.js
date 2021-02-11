a3232c40cf2b7b01c3371b142327ee9c7ea6e64c
n0y0j <n0y0j77@gmail.com>
2021-02-11 12:05:26 +0900

    feat: Purchase with Paypal

--- client/src/components/utils/Paypal.js
+++ client/src/components/utils/Paypal.js
@@ -24,11 +24,11 @@ export default class Paypal extends React.Component {
 
     let env = "sandbox"; // you can set here to 'production' for production
     let currency = "USD"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    let total = this.props.total; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: "YOUR-SANDBOX-APP-ID",
+      sandbox: "AYKXQW8WrLFwgv7q1960V7DSlNuifWHB9bro1tzPmX_ecWb31MXPWuynn8Vum52s9psbkaVLuBnrl-VJ",
       production: "YOUR-PRODUCTION-APP-ID",
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCartItems, removeCartItem } from "../../../_actions/user_actions";
-import UserCartBlock from "./Sections/UserCartBlock.js"
-import { Empty } from 'antd';
-import Paypal from '../../utils/Paypal'
-
+import UserCartBlock from "./Sections/UserCartBlock.js";
+import { Empty } from "antd";
+import Paypal from "../../utils/Paypal";
 
 function CartPage(props) {
   const dispatch = useDispatch();
 
-  const [Total, setTotal] = useState(0)
-  const [ShowTotal, setShowTotal] = useState(false)
+  const [Total, setTotal] = useState(0);
+  const [ShowTotal, setShowTotal] = useState(false);
 
   useEffect(() => {
     let cartItems = [];
@@ -18,37 +17,37 @@ function CartPage(props) {
     // redux의 user state cart안에 상품이 있는지 확인
     if (props.user.userData && props.user.userData.cart) {
       if (props.user.userData.cart.length > 0) {
-        props.user.userData.cart.forEach(item => {
+        props.user.userData.cart.forEach((item) => {
           cartItems.push(item.id);
         });
 
-        dispatch(getCartItems(cartItems, props.user.userData.cart))
-          .then(response => { calculateTotal(response.payload) });
+        dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
+          (response) => {
+            calculateTotal(response.payload);
+          }
+        );
       }
     }
   }, [props.user.userData]);
 
   let calculateTotal = (cartDetail) => {
-      let total = 0;
+    let total = 0;
 
-      cartDetail.map(item => {
-        total += parseInt(item.price, 10) * item.quantity;
-      })
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
 
-      setTotal(total)
-      setShowTotal(true)
-  }
+    setTotal(total);
+    setShowTotal(true);
+  };
 
   let removeFromCart = (productId) => {
-
-    dispatch(removeCartItem(productId))
-      .then(response => {
-          
-        if (response.payload.productInfo.length <= 0) {
-          setShowTotal(false)
-        }
-      })
-  }
+    dispatch(removeCartItem(productId)).then((response) => {
+      if (response.payload.productInfo.length <= 0) {
+        setShowTotal(false);
+      }
+    });
+  };
 
   return (
     <div style={{ width: "85%", margin: "3rem auto" }}>
@@ -71,8 +70,7 @@ function CartPage(props) {
         </>
       )}
 
-        <Paypal />
-
+      {ShowTotal && <Paypal total={Total} />}
     </div>
   );
 }
