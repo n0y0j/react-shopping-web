c522f63cbe66c7efa0abec20f341c46f0b321a38
n0y0j <n0y0j77@gmail.com>
2021-02-12 14:07:54 +0900

    feat: Implement History page.

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,48 +1,51 @@
-import React, { useState } from 'react'
-import { Collapse, Checkbox } from 'antd';
+import React, { useState } from "react";
+import { Collapse, Checkbox } from "antd";
 
 const { Panel } = Collapse;
 
 function CheckBox(props) {
+  const [Checked, setChecked] = useState([]);
 
-  const [Checked, setChecked] = useState([])
+  const handleToggle = (value) => {
+    // 누른 것의 Index를 구하고
+    const currentIndex = Checked.indexOf(value);
 
-    const handleToggle = (value) => {
+    // 전체 Checked된 State에서 현재 누른 Checkbox가 이미 있다면
+    const newChecked = [...Checked];
 
-      // 누른 것의 Index를 구하고
-      const currentIndex = Checked.indexOf(value)
-
-      // 전체 Checked된 State에서 현재 누른 Checkbox가 이미 있다면
-      const newChecked = [...Checked]
-
-      // State에 넣음
-      if (currentIndex === -1) {
-        newChecked.push(value)
-         // 빼주고
-      } else {
-        newChecked.splice(currentIndex, 1)
-      }
-      
-      setChecked(newChecked)
-      props.handleFilters(newChecked)
+    // State에 넣음
+    if (currentIndex === -1) {
+      newChecked.push(value);
+      // 빼주고
+    } else {
+      newChecked.splice(currentIndex, 1);
     }
 
-    const renderCheckboxLists = () => props.list && props.list.map((value, index) => (
-        <React.Fragment key={index}>
-            <Checkbox onChange={() => handleToggle(value._id)} checked={Checked.indexOf(value._id) === -1 ? false : true} /><span>{value.name}</span>
-        </React.Fragment>
-    ))
+    setChecked(newChecked);
+    props.handleFilters(newChecked);
+  };
 
-    return (
-      <div>
-        <Collapse defaultActiveKey={["0"]}>
-          <Panel header="Continents" key="1">
-            {renderCheckboxLists()}
-          </Panel>
-        </Collapse>
-        
-      </div>
-    );
+  const renderCheckboxLists = () =>
+    props.list &&
+    props.list.map((value, index) => (
+      <React.Fragment key={index}>
+        <Checkbox
+          onChange={() => handleToggle(value._id)}
+          checked={Checked.indexOf(value._id) === -1 ? false : true}
+        />
+        <span>{value.name}</span>
+      </React.Fragment>
+    ));
+
+  return (
+    <div>
+      <Collapse defaultActiveKey={["0"]}>
+        <Panel header="Continents" key="1">
+          {renderCheckboxLists()}
+        </Panel>
+      </Collapse>
+    </div>
+  );
 }
 
-export default CheckBox
+export default CheckBox;
